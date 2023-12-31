eb40f9cccf5ee7c16c0b0eb0b80b8952347f0c6b
Limon Islam <limon4u714@gmail.com>
2023-12-31 11:37:20 +0600

    vuejs learn

---
+++ src/components/About.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name:'About',
+    data(){
+        return{
+            email:"limonbabu165@gmail.com",
+            name:"Limon Islam",
+            getAge:function(a){
+                return a
+            }
+        }
+    }
+}
+</script>
+
+<template>
+ <h1> Coming from About Page {{ 20+3 }} {{ "hello Limon".length }} {{ email }} <br> name : {{ name == "Mony" }}</h1>
+ <h3>{{ getAge("Mehebuba") }}</h3>
+</template>
+
+<style scoped>
+h1{
+    color: chartreuse;
+    background-color: black;
+    padding: 20px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+<template>
+    <h1> Hello Home component !</h1>
+</template>
