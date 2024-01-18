5f1a859cdd50f9214b5982f1a9cbd74196165e54
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-18 23:06:17 +0600

    Create Child.vue

---
+++ src/components/Child.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'Child',
+    props:{
+        getIn:Function,
+        name:String,
+        nam:String,
+        
+       
+    }
+    
+}
+</script>
+<template>
+<h1> {{ name }}</h1>
+<h1> {{ nam }}</h1>
+<button v-on:click="getIn()">Get Information</button>
+</template>
