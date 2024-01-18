825d62a9058542b9914827b1f53fd5474ac10c2a
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-18 21:04:03 +0600

    Create About5.vue

---
+++ src/components/About5.vue
@@ -0,0 +1,69 @@
+
+<template>
+<li v-for="info in Games" :key="info">
+    {{ info }}
+
+</li>
+
+<h2 v-for="details in Employees" :key="details"> 
+    {{ details.name }} is {{ details.age }} years old.
+
+</h2>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data() {
+        return {
+           Games:["Cricket","Football","Tenis","Volley"],
+           Employees:[
+            {
+                name :"Rahim",
+                age:23
+            },
+            
+            {
+                name :"Tahim",
+                age:33
+            },
+            {
+                name :"Jahim",
+                age:43
+            },
+            {
+                name :"Lahim",
+                age:25
+            },
+            {
+                name :"Karim",
+                age:28
+            },
+
+           ]
+        }
+    },
+
+
+
+    methods: {
+       getInfo(){
+     
+       }
+    }
+}
+</script>
+
+
+
+
+
+<style scoped>
+h2 {
+    color: chartreuse;
+    background-color: black;
+    margin-top: 10px;
+    padding: 10px;
+    text-align: center;
+}
+</style>
