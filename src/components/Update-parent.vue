2cefe14c92a9f768724591b12789987bdd8d4421
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-18 23:35:17 +0600

    Create Update-parent.vue

---
+++ src/components/Update-parent.vue
@@ -0,0 +1,45 @@
+
+<template>
+
+<Child :pro="profession" :info="getInfo"/>
+
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+    name: 'About',
+    components:{
+        Child
+    },
+    data() {
+        return {
+          
+          profession:"Developer"
+         
+        }
+    },
+
+
+
+    methods: {
+       getInfo(){
+         alert("Everyday is beautiful")
+       }
+    }
+}
+</script>
+
+
+
+
+
+<style scoped>
+h2 {
+    color: chartreuse;
+    background-color: black;
+    margin-top: 10px;
+    padding: 10px;
+    text-align: center;
+}
+</style>
