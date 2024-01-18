ac7f4c0cfac8aaf70ae2a018a319b61bd3b85732
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-18 13:53:50 +0600

    Create About2.vue

---
+++ src/components/About2.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'About',
+    data() {
+        return {
+           name:null,
+           age:null
+        }
+    },
+    methods: {
+       getInfo(){
+       console.log(this.name, "is", this.age ,"years old")
+       }
+    }
+}
+</script>
+
+<template>
+
+<input type="text" v-model="name" >
+<br>
+<input type="number" v-model="age" >
+<br>
+<button type="submit" v-on:click = "getInfo()"> Submit </button>
+
+
+</template>
+
+
+<style scoped>
+button {
+    color: chartreuse;
+    background-color: black;
+    margin-top: 10px;
+    padding: 10px;
+}
+</style>
