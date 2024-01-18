9b6aa8d5882fedc9b9371f13a256ff2e3a2059b5
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-18 20:07:34 +0600

    Create About4.vue

---
+++ src/components/About4.vue
@@ -0,0 +1,31 @@
+
+<template>
+<h1 v-if="check">Condition presence</h1>
+<h1 v-else>Condition absence</h1>
+<button v-on:click="check=!check">Toggle</button>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data() {
+        return {
+         check:true
+        }
+    },
+
+
+
+    methods: {
+       getInfo(){
+     
+       }
+    }
+}
+</script>
+
+
+
+<style>
+
+</style>
