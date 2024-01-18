cb21fdcd96570fda98ff40f73a86fdd167ad884f
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-18 23:35:53 +0600

    Create Updae-child.vue

---
+++ src/components/Updae-child.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'Child',
+    props:{
+        name:String,
+        pro:Object,
+        info:Function
+    }
+    
+    
+}
+</script>
+
+
+<template>
+<h1> I am a {{ pro}} </h1>
+<button v-on:click="info()">Information</button>
+
+
+</template>
