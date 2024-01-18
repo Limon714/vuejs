2ddcadfda39d59d656c8351d05ee1cb9eadf13f5
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-18 23:05:46 +0600

    Create Parent.vue

---
+++ src/components/Parent.vue
@@ -0,0 +1,46 @@
+
+<template>
+<Child name="Rana"/>
+<Child  name="kana"/>
+<Child  :nam="nami" :getIn="getInfo"/>
+
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+    name: 'About',
+    components:{
+        Child
+    },
+    data() {
+        return {
+           nami:"Lion",
+          
+         
+        }
+    },
+
+
+
+    methods: {
+       getInfo(){
+         alert ("funciton called")
+       }
+    }
+}
+</script>
+
+
+
+
+
+<style scoped>
+h2 {
+    color: chartreuse;
+    background-color: black;
+    margin-top: 10px;
+    padding: 10px;
+    text-align: center;
+}
+</style>
