66a9ef21f2d7835e424b068f5bac46ad4d1a2ea1
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-19 21:26:45 +0600

    Create Child2.vue

---
+++ src/components/Child2.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'Child',
+    props:{
+        getInfo:Function
+    },
+    data(){
+        return {
+            Username:"Rahim"
+        }
+    }
+    
+}
+</script>
+
+
+<template>
+
+<button v-on:click="getInfo(Username)">Information</button>
+
+
+</template>
