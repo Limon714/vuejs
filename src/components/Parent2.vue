5cc06e192ac2afcc82961e375e04ed0ed73f39de
Limon Islam <72975868+Limon714@users.noreply.github.com>
2024-01-19 21:26:17 +0600

    Create Parent2.vue

---
+++ src/components/Parent2.vue
@@ -0,0 +1,42 @@
+
+<template>
+<h1> User : {{ name }}</h1>
+<Child :getInfo="getInfo"/>
+
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+    name: 'About',
+    components:{
+        Child
+    },
+    data() {
+        return {
+          
+            name:''
+         
+        }
+    },
+
+
+
+    methods: {
+       getInfo(name){
+        this.name=name
+       }
+    }
+}
+</script>
+
+
+<style scoped>
+h2 {
+    color: chartreuse;
+    background-color: black;
+    margin-top: 10px;
+    padding: 10px;
+    text-align: center;
+}
+</style>
